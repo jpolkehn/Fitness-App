624cad1375d0fe5b811f0635289e75922dd2924f
Jörg Polkehn <jpolkehn@gmail.com>
2024-04-24 20:14:13 +0200

    gradient 2 mit 1 getauscht

--- src/components/colorWheel.js
+++ src/components/colorWheel.js
@@ -1,7 +1,7 @@
 // Liste von Klassen
 const classNamesList = [
-  "block bg-gradient-to-br from-fitness-color-turkis to-fitness-color-light-green text-white text-3xl my-4 pt-16 pb-8 h-44 w-full rounded-3xl",
   "block bg-gradient-to-br from-fitness-color-yellow to-fitness-color-light-pink text-white text-3xl my-4 pt-16 pb-8 h-44 w-full rounded-3xl",
+  "block bg-gradient-to-br from-fitness-color-turkis to-fitness-color-light-green text-white text-3xl my-4 pt-16 pb-8 h-44 w-full rounded-3xl",
   "block bg-gradient-to-br from-fitness-color-dark-blue to-fitness-color-mint text-white text-3xl my-4 pt-16 pb-8 h-44 w-full rounded-3xl",
 ];
 
